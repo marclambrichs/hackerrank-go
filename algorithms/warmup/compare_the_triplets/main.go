30777e81392e20ebcdd504e5ef094f6f357567d4
Marc Lambrichs <marc.lambrichs@gmail.com>
2017-10-17 05:41:05 +0200

    Add compare the triplets

---
+++ algorithms/warmup/compare_the_triplets/main.go
@@ -0,0 +1,28 @@
+package main
+
+import "fmt"
+
+func scanInt(n int) []int {
+	input := make([]int, n)
+	buffer := make([]interface{}, n)
+
+	for i := 0; i < n; i++ {
+		buffer[i] = &input[i]
+	}
+	fmt.Scanln(buffer...)
+	return input
+}
+
+func main() {
+	var alice, bob int
+	a := scanInt(3)
+	b := scanInt(3)
+	for i, _ := range a {
+		if a[i] > b[i] {
+			alice++
+		} else if a[i] < b[i] {
+			bob++
+		}
+	}
+	fmt.Printf("%d %d\n", alice, bob)
+}
